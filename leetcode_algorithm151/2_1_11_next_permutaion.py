c5f2c2a2126914d23eace55c6016a5afd164b1e8
836304831@qq.com <836304831@qq.com>
2020-03-25 23:30:41 +0800

    udpate

--- leetcode_algorithm151/2_1_11_next_permutaion.py
+++ leetcode_algorithm151/2_1_11_next_permutaion.py
@@ -49,5 +49,6 @@ if __name__ == "__main__":
     # nums = [2, 1, 0, 0]
     # nums = [1, 3, 2]
     # nums = [3, 2, 1]
+
     result = Solution().nextPermutation(nums)
     print("result: {}".format(nums))
